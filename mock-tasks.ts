8d7a9a9aff4695c7747e27a6f98f97c4a8347440
NanaPhiona <nankyaphio15@gmail.com>
2023-08-26 03:50:52 +0300

    Learning Angular

---
+++ mock-tasks.ts
@@ -0,0 +1,23 @@
+import { Task } from "Task";
+
+export const TASKS: Task[]= [
+    {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "August 29th at 3:30pm",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Meeting at School",
+        day: "August 30th at 1:30pm",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: "September 1 at 10:00am",
+        reminder: false,
+    },
+
+];
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from 'Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent {
+  @Input() task: Task;
+  @Output() OnDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() OnToggleReminder: EventEmitter<Task> = new EventEmitter()
+  faTimes =faTimes;
+
+  onDelete(task:any){
+    this.OnDeleteTask.emit(task)
+  }
+  
+  onToggle(task:any){
+    this.OnToggleReminder.emit(task)
+  }
+}
